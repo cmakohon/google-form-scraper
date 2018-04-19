c9be5acc1b22739de2925b8f7067aae52e01fce6
cmakohon <cmakohon@me.com>
2018-04-19 13:29:45 -0400

    removed useless route

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import HomePage from "./components/home-page/HomePage";
-import Scraper from "./components/scraper/Scraper";
 
 export default () => {
   return (
     <Switch>
       <Route exact path='/' component={HomePage}/>
-      <Route exact path='/scraper' component={Scraper}/>
     </Switch>
   )
 }
